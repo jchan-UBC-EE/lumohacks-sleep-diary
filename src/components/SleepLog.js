1e1d202cdee7a50a9c5688c62ef41b999e2a0dbe
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-17 12:30:15 -0700

    getdate on form

--- src/components/SleepLog.js
+++ src/components/SleepLog.js
@@ -31,107 +31,120 @@ export default class Layout extends Component {
 		this.setWakeTime = this.setWakeTime.bind(this);
 		this.setTimeToGetOutOfBed = this.setTimeToGetOutOfBed.bind(this);
 		this.setComments = this.setComments.bind(this);
+		this.getDate = this.getDate.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
+	componentDidMount() {
+		this.getDate();
+	}
+
+	getDate() {
+		const today = new Date();
+		const getDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+		this.setState({ CreateDate: getDate });
+	}
+
 	setNapsDuringDay(event) {
-		this.setState({NapsDuringDay: event.target.value})
+		this.setState({ NapsDuringDay: event.target.value })
 	}
 
 	setMedication(event) {
-		this.setState({Medication: event.target.value})
+		this.setState({ Medication: event.target.value })
 	}
 
 	setTimeInBed(event) {
-		this.setState({TimeInBed: event.target.value})
+		this.setState({ TimeInBed: event.target.value })
 	}
 
 	setTimeTryToSleep(event) {
-		this.setState({TimeTryToSleep: event.target.value})
+		this.setState({ TimeTryToSleep: event.target.value })
 	}
 
 	setHowLongToSleep(event) {
-		this.setState({HowLongToSleep: event.target.value})
+		this.setState({ HowLongToSleep: event.target.value })
 	}
 
 	setAmountWakeUp(event) {
-		this.setState({AmountWakeUp: event.target.value})
+		this.setState({ AmountWakeUp: event.target.value })
 	}
 
 	setHowLongDidYouSleep(event) {
-		this.setState({HowLongDidYouSleep: event.target.value})
+		this.setState({ HowLongDidYouSleep: event.target.value })
 	}
 
 	setWakeTime(event) {
-		this.setState({WakeTime: event.target.value})
+		this.setState({ WakeTime: event.target.value })
 	}
 
 	setTimeToGetOutOfBed(event) {
-		this.setState({TimeToGetOutOfBed: event.target.value})
+		this.setState({ TimeToGetOutOfBed: event.target.value })
 	}
 
 	setComments(event) {
-		this.setState({Comments: event.target.value})
+		this.setState({ Comments: event.target.value })
 	}
 
 	handleSubmit(event) {
 		event.preventDefault()
-        fetch('/api/sign-up', {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(this.state)
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-                if (json.confirmed === true) {
-                    window.location.replace("/");
-                } else {
-                    document.getElementById("error").innerHTML = "please try again";
-                }
-            })
+		fetch('/api/sleep-log', {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: "POST",
+			body: JSON.stringify(this.state)
+		})
+			.then(response => response.json())
+			.then(json => {
+				console.log(json)
+				if (json.confirmed === true) {
+					window.location.replace("/");
+				} else {
+					document.getElementById("error").innerHTML = "please try again";
+				}
+			})
 	}
 
 	render() {
 		return (
 			<form
-				id="sleep_diary_form" 
+				id="sleep_diary_form"
 				onSubmit={this.handleSubmit}>
-				
 				<h4>Please fill this section out in the evening, before going to bed.</h4>
 				Enter the time and duration of any naps you took today:<br />
+				<div >
+					<input type="text" name="date" value={this.state.CreateDate} readonly />
+				</div>
 				<div className="nap inputWrapper">
-					<input className="napTime" 
-						type="time" 
+					<input className="napTime"
+						type="time"
 						name="nap_time"
 						onChange={this.setNapsDuringDay} />
 				</div>
 				Did you take any medication to help you sleep? If so, what did you take and when?
-					<input 
-						type="text"
-						name="medication"
-						onChange={this.setMedication} />
+					<input
+					type="text"
+					name="medication"
+					onChange={this.setMedication} />
 				<br />
 				<h4>Please fill this section out as soon as you get up for the day the following morning.</h4>
 				What time did you get into bed?<br />
-				<input className="bedTime" 
-					type="time" 
+				<input className="bedTime"
+					type="time"
 					name="bed_time"
 					onChange={this.setTimeInBed} />
 				<br />
 				What time did you try to go to sleep?<br />
-				<input className="trySleepTime" 
-					type="time" 
+				<input className="trySleepTime"
+					type="time"
 					name="try_Sleep_time"
 					onChange={this.setTimeTryToSleep} />
 				<br />
 				How long did it take you to fall asleep?<br />
 				<div className="timeTryToSleep">
-					<input className="timeToFallAsleep" 
-						type="number" 
+					<input className="timeToFallAsleep"
+						type="number"
 						name="time_to_sleep"
 						onChange={this.setHowLongToSleep} />
 					<span className="timeToSleepText">min</span>
@@ -139,33 +152,33 @@ export default class Layout extends Component {
 				<br />
 				How many times did you wake up?
 				<br />
-				<input type="number" 
+				<input type="number"
 					name="time_wake_up"
 					onChange={this.setAmountWakeUp} />
 				<br />
 				In total, how long did you sleep?
 				<br />
-				<input type="number" 
+				<input type="number"
 					name="total_sleep_total"
 					onChange={this.setHowLongDidYouSleep} />
 				<br />
 				What time was your final awakening?
 				<br />
-				<input type="time" 
+				<input type="time"
 					name="final_awakening"
 					onChange={this.setWakeTime} />
 				<br />
 				What time did you get out of bed for the day?
 				<br />
-				<input type="time" 
+				<input type="time"
 					name="time_out_of_bed"
 					onChange={this.setTimeToGetOutOfBed} />
 				<br />
 				Comments (if applicable)
 				<br />
 				<textarea name="comment" form="sleep_diary_form">Comments (if applicable)</textarea>
-				<input className="btn" 
-					type="submit" 
+				<input className="btn"
+					type="submit"
 					value="Submit" />
 			</form>
 		);

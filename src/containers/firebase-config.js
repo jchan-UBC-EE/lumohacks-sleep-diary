a8511eda40ca53bf0d55623114e4502fab8b6d7c
Patrick Lin <patrick@patricklin.ca>
2019-01-07 08:42:10 -0800

    Update firebase-config.js

--- src/containers/firebase-config.js
+++ src/containers/firebase-config.js
@@ -1,12 +1,6 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: 'AIzaSyDB276j55J4kvu9gNCR8LaLc4AbJha4CWM',
-  authDomain: 'lumohacks-sleep-diary.firebaseapp.com',
-  databaseURL: 'https://lumohacks-sleep-diary.firebaseio.com',
-  projectId: 'lumohacks-sleep-diary',
-  storageBucket: 'lumohacks-sleep-diary.appspot.com',
-  messagingSenderId: '944607255435'
 }
 
 firebase.initializeApp(config)
